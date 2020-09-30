7f775f7c854ae865ad42f415fabc0a76207ea562
nkleinmann <nekleinmann@gmail.com>
2020-09-29 21:13:25 -0400

    Post and get requests work for user answers

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -18,104 +18,103 @@ $(function () {
   console.log("testing userID");
   console.log(userID);
 
-  let questionID;
+  let talentID = $talent.attr("data-questionid");
 
   $updateProfile.data("id", userID);
 
-
-  // Update user answers to questions in database
-  const updateTalentAnswer = (userID) => {
-    axios.put(`/api/userAnswers/${userID}`, {
-      id: userID,
-      QuestionId: $talent.attr("data-questionid"),
-      answer: $talent.val().trim()
-    })
-    .then((response) => {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log(error);
-    })
-  }
+  // // Update user answers to questions in database
+  // const updateTalentAnswer = (userID) => {
+  //   axios.put('/api/answer', {
+  //     TeamMemberId: userID,
+  //     QuestionId: talentID,
+  //     answer: $talent.val().trim()
+  //   })
+  //     .then((response) => {
+  //       console.log(response);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     })
+  // }
 
   // Post Answers
   const addTalentAnswer = (userID) => {
     // let questionID = $firstName.attr("data-id");
-    axios.post("/api/answer", 
-    {
-      TeamMemberId: userID,
-      QuestionId: $talent.attr("data-questionid"),
-      answer: $talent.val().trim()
+    axios.post("/api/answer",
+      {
+        TeamMemberId: userID,
+        QuestionId: talentID,
+        answer: $talent.val().trim()
 
-    }).then((response) => {
-      console.log(response);
-    })
+      }).then((response) => {
+        console.log(response);
+      })
   }
 
   const addPeeveAnswer = (userID) => {
     // let questionID = $firstName.attr("data-id");
-    axios.post("/api/answer", 
-    {
-      TeamMemberId: userID,
-      QuestionId: $peeve.attr("data-questionid"),
-      answer: $peeve.val().trim()
+    axios.post("/api/answer",
+      {
+        TeamMemberId: userID,
+        QuestionId: $peeve.attr("data-questionid"),
+        answer: $peeve.val().trim()
 
-    }).then((response) => {
-      console.log(response);
-    })
+      }).then((response) => {
+        console.log(response);
+      })
   }
 
 
   const addFoodAnswer = (userID) => {
     // let questionID = $firstName.attr("data-id");
-    axios.post("/api/answer", 
-    {
-      TeamMemberId: userID,
-      QuestionId: $food.attr("data-questionid"),
-      answer: $food.val().trim()
+    axios.post("/api/answer",
+      {
+        TeamMemberId: userID,
+        QuestionId: $food.attr("data-questionid"),
+        answer: $food.val().trim()
 
-    }).then((response) => {
-      console.log(response);
-    })
+      }).then((response) => {
+        console.log(response);
+      })
   }
 
   const addVehicleAnswer = (userID) => {
     // let questionID = $firstName.attr("data-id");
-    axios.post("/api/answer", 
-    {
-      TeamMemberId: userID,
-      QuestionId: $vehicle.attr("data-questionid"),
-      answer: $vehicle.val().trim()
+    axios.post("/api/answer",
+      {
+        TeamMemberId: userID,
+        QuestionId: $vehicle.attr("data-questionid"),
+        answer: $vehicle.val().trim()
 
-    }).then((response) => {
-      console.log(response);
-    })
+      }).then((response) => {
+        console.log(response);
+      })
   }
 
   const addTVAnswer = (userID) => {
     // let questionID = $firstName.attr("data-id");
-    axios.post("/api/answer", 
-    {
-      TeamMemberId: userID,
-      QuestionId: $tvShow.attr("data-questionid"),
-      answer: $tvShow.val().trim()
+    axios.post("/api/answer",
+      {
+        TeamMemberId: userID,
+        QuestionId: $tvShow.attr("data-questionid"),
+        answer: $tvShow.val().trim()
 
-    }).then((response) => {
-      console.log(response);
-    })
+      }).then((response) => {
+        console.log(response);
+      })
   }
 
   const addBookAnswer = (userID) => {
     // let questionID = $firstName.attr("data-id");
-    axios.post("/api/answer", 
-    {
-      TeamMemberId: userID,
-      QuestionId: $book.attr("data-questionid"),
-      answer: $book.val().trim()
+    axios.post("/api/answer",
+      {
+        TeamMemberId: userID,
+        QuestionId: $book.attr("data-questionid"),
+        answer: $book.val().trim()
 
-    }).then((response) => {
-      console.log(response);
-    })
+      }).then((response) => {
+        console.log(response);
+      })
   }
 
   // Put/Update Answers
@@ -128,8 +127,8 @@ $(function () {
     });
   };
 
-   // Get Team Names for the drop-down
-   const getTeams = () => {
+  // Get Team Names for the drop-down
+  const getTeams = () => {
     axios.get("/api/team")
       .then((response) => {
 
@@ -167,15 +166,18 @@ $(function () {
   // Add user answers to screen when refreshed
   const repopulateAnswerInfo = (userID) => {
     axios.get(`/api/userAnswers/${userID}`)
-    .then((response) => {
-      console.log(response);
-      $talent.val(response.data[0].answer);
-      $peeve.val(response.data[1].answer);
-      $food.val(response.data[2].answer);
-      $vehicle.val(response.data[3].answer);
-      $tvShow.val(response.data[4].answer);
-      $book.val(response.data[5].answer);
-    })
+      .then((response) => {
+        console.log(response);
+        $talent.val(response.data[0].answer);
+        $peeve.val(response.data[1].answer);
+        $food.val(response.data[2].answer);
+        $vehicle.val(response.data[3].answer);
+        $tvShow.val(response.data[4].answer);
+        $book.val(response.data[5].answer);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
   }
 
   // Add user info to screen when refreshed
@@ -193,7 +195,7 @@ $(function () {
       });
   }
 
-  
+
 
 
   getTeams();
@@ -201,18 +203,30 @@ $(function () {
   repopulateAnswerInfo(userID);
   // console.log("User ID from getUser")
   getUser(userID);
-  
 
 
- $submitForm.on("submit", function (event) {
+
+  $submitForm.on("submit", function (event) {
     event.preventDefault();
     // let btnUserID = $firstName.attr("data-id");
     console.log("userID from save button");
     console.log(userID);
-    //conditions for adding info to database
-    // if 
-    // updateTalentAnswer(userID);
+
     addUserInfo(userID);
+
+    //conditions for adding info to database
+
+    // if ($talent.val()) {
+    //   updateTalentAnswer(userID);
+    //   console.log("should be updating!");
+    // }
+    // else {
+    //   addTalentAnswer(userID);
+    //   console.log("adding talent!")
+    // }
+
+
+    // updateTalentAnswer(userID);
     addTalentAnswer(userID);
     addPeeveAnswer(userID);
     addFoodAnswer(userID);
@@ -223,7 +237,7 @@ $(function () {
 
   });
 
-  
+
 
 });
 
