6308ebe120e9456c76e6c2e3b37b64b461c30d19
nkleinmann <nekleinmann@gmail.com>
2020-09-30 22:39:26 -0400

    modal closes when save button clicked

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -10,18 +10,13 @@ $(function () {
             name: newTeamVal
         }).then((response) => {
             console.log(response);
+            window.location.reload();
         })
     }
-
-    // const viewAllTeams = () => {
-    //     axios.get("/api/team", {
-            
-    //     })
-    // }
 
     saveNewTeamBtn.on("click", function() {
         newTeamVal = newTeamName.val().trim();
         addNewTeamName();
-        
+         $("#createTeamModal").modal("hide");       
     })
 })
