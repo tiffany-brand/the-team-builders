4b07b25ae4a08cd621e965bea3a3a5ed48710a56
hilbug <65197724+hilbug@users.noreply.github.com>
2020-10-01 19:53:47 -0400

    start card number at 1

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -159,7 +159,8 @@ $(function () {
 
     // Append cards to deck
     for (let i = 0; i < allCards.length; i++) {
-      let idDiv = $("<div>").text(`${i}`).addClass("text-center");
+      let cardNum = i+1;
+      let idDiv = $("<div>").text(`${cardNum}`).addClass("text-center");
       let cardDiv = $(`<li class="card gamecard text-wrap" data-id=${allCards[i].id}">${allCards[i].cardvalue}</li>`);
       let cardIDDiv = idDiv.append(cardDiv);
       $deck.append(cardIDDiv);
